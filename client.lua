63484c598fe106d4a832e986780f157189474a5f
fribblet56 <fribblet56@gmail.com>
2020-04-23 01:03:09 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+AddEvent("OnPlayerEnterWater", function()
+
+    local x, y, z = GetPlayerLocation()
+    
+    local tryZ = 0
+    local maxTry = 60
+    local terrain_height = GetTerrainHeight(x, y, tryZ)
+
+
+    while terrain_height == false or terrain_height <= 100 do
+        terrain_height = GetTerrainHeight(x, y, tryZ)
+        tryZ = tryZ + 100
+        if tryZ > maxTry * 100 then return end
+    end
+
+    if terrain_height >= 100 then
+    	CallRemoteEvent("UnstuckPlayer", terrain_height)
+    end
+
+end)
